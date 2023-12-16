e0ef9d0bc6f8b4e0ff51a6ac152926208a6452dc
Gabriel Lima <gabriel.limabs02@gmail.com>
2023-12-16 02:43:13 -0300

    feat: add more boards and remove useless search button

--- app/ui/navigation.tsx
+++ app/ui/navigation.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 
 export const Navigation = () => {
   const [mobileMenuVisible, setMobileMenuVisible] = useState(false)
-  const boards = ['b', 'a', 'int', 'pol', 'v', 'lit', 'x', 'his', 'r9k']
+  const boards = ['b', 'fit', 'k', 'a', 'int', 'pol', 'v', 'sci', 'lit', 'x', 'his', 'r9k', 'wg']
 
   return (
     <nav className="bg-slate-400">
@@ -31,15 +31,6 @@ export const Navigation = () => {
               ))}
             </div>
           </div>
-
-          <div className="hidden md:flex items-center space-x-1">
-            <div className="cursor-pointer py-4 px-3 hover:text-gray-900 transition duration-300">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-              </svg>
-            </div>
-          </div>
-
           <div className="md:hidden flex items-center">
             <button
               onClick={() => setMobileMenuVisible(!mobileMenuVisible)}
