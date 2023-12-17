2b5df03c4edcf1e704b634438e38a1ea94673d7c
Gabriel Lima <gabriel.limabs02@gmail.com>
2023-12-17 20:47:44 -0300

    feat: add link to github in navbar

--- app/ui/navigation.tsx
+++ app/ui/navigation.tsx
@@ -32,6 +32,21 @@ export const Navigation = () => {
               ))}
             </div>
           </div>
+          <div className="hidden md:flex items-center space-x-1">
+            <div className="cursor-pointer py-4 px-3 hover:text-gray-900 transition duration-300">
+              <a 
+                className="flex items-center gap-2"
+                href="https://github.com/splorg/kuroki-chan" 
+                target="_blank"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                </svg>
+                <span className="hidden lg:inline">Source</span>
+              </a>
+
+            </div>
+          </div>
           <div className="md:hidden flex items-center">
             <button
               onClick={() => setMobileMenuVisible(!mobileMenuVisible)}
@@ -42,7 +57,6 @@ export const Navigation = () => {
               </svg>
             </button>
           </div>
-
         </div>
       </div>
 
@@ -56,6 +70,16 @@ export const Navigation = () => {
             /{board}/
           </Link>
         ))}
+        <a 
+          className="flex py-2 px-4 text-sm items-center gap-2 hover:bg-slate-600 transition duration-300"
+          href="https://github.com/splorg/kuroki-chan" 
+          target="_blank"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+          </svg>
+          <span>Source</span>
+        </a>
       </div>
     </nav>
   )
