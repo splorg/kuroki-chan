75cf8d504e47a6c28aadc3febcf805c3bb8d2894
Gabriel Lima <gabriel.limabs02@gmail.com>
2023-12-23 03:40:08 -0300

    feat: full-size file viewing and webm support

--- app/ui/post.tsx
+++ app/ui/post.tsx
@@ -1,7 +1,12 @@
+'use client'
+
+import useLightbox from "@/hooks/useLightbox"
 import { generateThumbnailUrl } from "@/utils/generateThumbnailUrl"
 import { dateStringFromTimestamp } from "@/utils/dateStringFromTimestamp"
 
 import { Post } from "../lib/definitions"
+import { generateFileUrl } from "@/utils/generateFileUrl"
+import { SlideImage, SlideVideo } from "yet-another-react-lightbox"
 
 type Props = {
   post: Post
@@ -9,16 +14,40 @@ type Props = {
 }
 
 const Post = ({ post, board }: Props) => {
+  const { openLightbox, renderLightbox } = useLightbox()
+  const hasFile = post.tim && post.ext
+
+  const imageSlide: SlideImage[] = [
+    {
+      type: 'image',
+      src: generateFileUrl(post, board)
+    }
+  ]
+
+  const videoSlide: SlideVideo[] = [
+    {
+      type: 'video',
+      sources: [
+        { src: generateFileUrl(post, board), type: 'video/webm' }
+      ],
+      controls: true,
+      autoPlay: true,
+      loop: true,
+    }
+  ]
+
   return (
     <div id={`p${post.no}`} className="rounded-lg bg-slate-700 w-fit p-4 flex flex-col md:flex-row gap-8 justify-evenly">
-    {post.tim && post.ext && post.ext !== '.webm' ? (
+    {hasFile ? (
       <div>
         <img
           src={generateThumbnailUrl(post, board)}
           alt={`${post.tim}${post.ext}`}
           width={post.tn_w}
           height={post.tn_h}
+          onClick={openLightbox}
           loading="lazy"
+          className="cursor-pointer"
         />
       </div>
     ) : null}
@@ -36,6 +65,19 @@ const Post = ({ post, board }: Props) => {
         />
       ) : null}
     </div>
+
+    {renderLightbox({ 
+      slides: post.ext! === '.webm' ? videoSlide : imageSlide, 
+      render: {
+        buttonPrev: () => null,
+        buttonNext: () => null,
+      },
+      video: {
+        controls: true,
+        autoPlay: true,
+        loop: true
+      },
+    })}
   </div>
   )
 }
--- utils/generateFileUrl.ts
+++ utils/generateFileUrl.ts
@@ -1,6 +1,6 @@
 import { Post } from "@/app/lib/definitions";
 
-export const generateImageUrl = (post: Post, board: string) => {
+export const generateFileUrl = (post: Post, board: string) => {
   if (!post.tim || !post.ext) return ''
 
   return `/api/image?board=${board}&tim=${post.tim}&ext=${post.ext}`
