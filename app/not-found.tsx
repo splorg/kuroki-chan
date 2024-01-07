9dd809833f91765072ce3b767a3d0cbd55b143cf
Gabriel Lima <gabriel.limabs02@gmail.com>
2024-01-07 02:36:05 -0300

    fix: replace next/image component with img tag

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,18 +1,15 @@
-import Image from 'next/image'
-
 const NotFound = () => {
   return (
     <main
       className='flex items-center justify-center flex-col gap-4 mx-auto min-h-custom'
     >
       <h1 className='text-xl'>not found!</h1>
-      <Image 
+      <img
         src="/not-found.webp"
         alt='Confused Kuroki-chan.'
         width={330}
         height={330}
         className='outline outline-2 outline-white'
-        unoptimized
       />
     </main>
   )
