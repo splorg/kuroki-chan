ab5b87f71bb13ae36e137bba40a76a53e4b2b0b8
Gabriel Lima <gabriel.limabs02@gmail.com>
2024-01-07 04:15:38 -0300

    style: indentation and line break

--- app/ui/content-wrapper.tsx
+++ app/ui/content-wrapper.tsx
@@ -19,20 +19,21 @@ const ContentWrapper = async () => {
       </Suspense>
     )
   }
+  
   const content = await getBoard(board, Number(page))
   const threadIds = content.map(thread => thread.posts[0].no)
 
   return (
-      <Suspense fallback={<ContentSkeleton />}>
-        <section className="mt-4 mb-8 flex flex-col gap-3">
-          {threadIds.map(thread => (
-            <Thread key={thread} id={thread.toString()} />
-          ))}
-        </section>
-        <div className="p-4 mb-8">
-          <Pagination pages={10} />
-        </div>
-      </Suspense>
+    <Suspense fallback={<ContentSkeleton />}>
+      <section className="mt-4 mb-8 flex flex-col gap-3">
+        {threadIds.map(thread => (
+          <Thread key={thread} id={thread.toString()} />
+        ))}
+      </section>
+      <div className="p-4 mb-8">
+        <Pagination pages={10} />
+      </div>
+    </Suspense>
   )
 }
 
